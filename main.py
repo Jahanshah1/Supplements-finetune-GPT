91fc2868e849e62c2cdb0f698edda337ac760e35
Jahan Shah <shahjahanrajan@gmail.com>
2023-05-31 15:20:24 +0530

    GUI added

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ model_name = 'openai api fine_tunes.follow -i ft-Cb0l7ZSm7QKFKxbePal8wcA2'
 model_davinci = 'openai api completions.create -m davinci:ft-personal-2023-04-30-03-50-36 -p <YOUR_PROMPT>'
 
 sample_prompt = 'openai api completions.create -m davinci:ft-personal-2023-04-30-03-50-36 -p 'which is the best whey supplement for athletes' --max-tokens 100'
+
+export OPENAI_API_KEY="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"
